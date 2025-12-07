7888a18656886560c738e1f5016d8dc8bf1d0917
Christophe KUNZLI <christophe.kunzli@heig-vd.ch>
2025-12-07 12:37:34 +0100

    Day 7 part 2

--- src/Day7.java
+++ src/Day7.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.math.BigInteger;
 
 public class Day7 {
 
@@ -91,15 +90,42 @@ public class Day7 {
         return count;
     }
 
-    private int part2() {
-        for (char[] row : copyGrid) {
-            for (char col : row) {
-                System.out.print(col);
+    private long part2() {
+        Map<Coordinate, Long> beams = new HashMap<>();
+        beams.put(new Coordinate(startRow, startCol), 1L);
+
+        long paths = 0L;
+
+        while (!beams.isEmpty()) {
+            Map<Coordinate, Long> next = new HashMap<>();
+
+            for (Map.Entry<Coordinate, Long> current : beams.entrySet()) {
+                Coordinate cur = current.getKey();
+                long count = current.getValue();
+
+                int nextRow = cur.row + 1;
+                int col = cur.col;
+
+                while (nextRow < grid.length && grid[nextRow][col] == EMPTY) {
+                    nextRow++;
+                }
+
+                if (nextRow >= grid.length) {
+                    paths += count;
+                } else if (grid[nextRow][col] == SPLITTER) {
+                    int leftCol = col - 1;
+                    int rightCol = col + 1;
+                    Coordinate left = new Coordinate(nextRow, leftCol);
+                    next.merge(left, count, Long::sum);
+                    Coordinate right = new Coordinate(nextRow, rightCol);
+                    next.merge(right, count, Long::sum);
+                }
             }
-            System.out.println();
+
+            beams = next;
         }
 
-        return 0;
+        return paths;
     }
 
     private void parse(List<String> input) {
