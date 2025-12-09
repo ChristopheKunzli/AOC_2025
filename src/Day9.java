daa4899dc5be1c91f64a45db5e7358938ce90404
Christophe KUNZLI <christophe.kunzli@heig-vd.ch>
2025-12-09 18:00:42 +0100

    Day 9 part 2

--- src/Day9.java
+++ src/Day9.java
@@ -3,16 +3,25 @@ import java.util.List;
 
 public class Day9 {
 
-    private record Coordinate(long row, long col) {
-        @Override
-        public String toString() {
-            return "(" + row + "," + col + ")";
+    private record Coordinate(long row, long col) {}
+
+    private record Rectangle(Coordinate c1, Coordinate c2) {
+        boolean hasOverlapWithLine(Rectangle line) {
+            return c1.col < line.c2.col && c2.col > line.c1.col && c1.row < line.c2.row && c2.row > line.c1.row;
         }
 
-        public static long rectangleArea(Coordinate corner1, Coordinate corner2) {
-            long length = Math.abs(corner1.row - corner2.row) + 1;
-            long width = Math.abs(corner1.col - corner2.col) + 1;
+        long area() {
+            long length = Math.abs(c1.row - c2.row) + 1;
+            long width = Math.abs(c1.col - c2.col) + 1;
             return length * width;
+        }
+
+        static Rectangle fromCorners(Coordinate corner1, Coordinate corner2) {
+            long minRow = Math.min(corner1.row, corner2.row);
+            long maxRow = Math.max(corner1.row, corner2.row);
+            long minCol = Math.min(corner1.col, corner2.col);
+            long maxCol = Math.max(corner1.col, corner2.col);
+            return new Rectangle(new Coordinate(minRow, minCol), new Coordinate(maxRow, maxCol));
         }
     }
 
@@ -42,14 +51,47 @@ public class Day9 {
             Coordinate corner1 = redTiles.get(i);
             for (int j = i + 1; j < redTiles.size(); ++j) {
                 Coordinate corner2 = redTiles.get(j);
-                maxArea = Math.max(Coordinate.rectangleArea(corner1, corner2), maxArea);
+                maxArea = Math.max(new Rectangle(corner1, corner2).area(), maxArea);
             }
         }
 
         return maxArea;
     }
 
-    private int part2(List<Coordinate> redTiles) {
-        return 0;
+    private long part2(List<Coordinate> redTiles) {
+        List<Rectangle> rectangles = new ArrayList<>();
+        for (int i = 0; i < redTiles.size() - 1; ++i) {
+            for (int j = i + 1; j < redTiles.size(); ++j) {
+                rectangles.add(Rectangle.fromCorners(redTiles.get(i), redTiles.get(j)));
+            }
+        }
+
+        List<Rectangle> lines = new ArrayList<>();
+
+        for (int i = 0; i < redTiles.size() - 1; ++i) {
+            Coordinate start = redTiles.get(i);
+            Coordinate end = redTiles.get((i + 1));
+            lines.add(Rectangle.fromCorners(start, end));
+        }
+        lines.add(Rectangle.fromCorners(redTiles.getLast(), redTiles.getFirst()));
+
+        long maxArea = 0;
+
+        for (Rectangle r : rectangles) {
+            boolean overlap = false;
+
+            for (Rectangle line : lines) {
+                if (r.hasOverlapWithLine(line)) {
+                    overlap = true;
+                    break;
+                }
+            }
+
+            if (!overlap) {
+                maxArea = Math.max(maxArea, r.area());
+            }
+        }
+
+        return maxArea;
     }
 }
